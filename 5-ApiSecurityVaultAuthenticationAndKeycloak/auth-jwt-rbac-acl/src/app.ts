d7ae6abc45ce7a370f386d2a1e67e59dba245773
Evelyn Cristini Oliveira Throssell <cristinioliveira.eo@gmail.com>
2025-06-10 13:59:26 +0000

    feat: jwt token generation

--- 5-ApiSecurityVaultAuthenticationAndKeycloak/auth-jwt-rbac-acl/src/app.ts
+++ 5-ApiSecurityVaultAuthenticationAndKeycloak/auth-jwt-rbac-acl/src/app.ts
@@ -2,6 +2,8 @@ import express, { NextFunction } from "express";
 import { logRequest, logResponse } from "./lib/log";
 import { loadFixtures } from "./fixtures";
 import { userRouter } from "./router/user-router";
+import { createDatabaseConnection } from "./database";
+import jwt from 'jsonwebtoken';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,18 +13,30 @@ app.use(logRequest);
 app.use(logResponse);
 
 app.use(
-    async(
+    async (
         error: Error,
         req: express.Request,
         res: express.Response,
         next: express.NextFunction
-        ) => {
+    ) => {
         if (!error) {
             return next();
         }
         errorHandler(error, req, res, next);
-        }
+    }
 );
+
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+    const { userRepository } = await createDatabaseConnection();
+    const user = await userRepository.findOne({ where: { email } });
+
+    if (!user || !user.comparePassword(password)) {
+        throw new Error('Invalid credentials')
+    }
+    const accessToken = jwt.sign({ name: user.name, email: user.email }, 'secret');
+    res.json({ accessToken })
+});
 
 app.use("", userRouter)
 app.use(errorHandler);
@@ -37,22 +51,32 @@ function errorHandler(
     req: express.Request,
     res: express.Response,
     next: express.NextFunction
-){
+) {
     if (!error) {
         return;
     }
 
-const errorDetails = {
-    name: error.name,
-    message: error.message,
-    stack: error.stack,
-    cause: error.cause,
-};
-console.error("Error details:", JSON.stringify(errorDetails, (key, value) => {
-    if (key === 'stack' && typeof value === 'string') {
-        return value.split('\n').map(line => line.trim());
-    } 
-    return value;
-}, 2))
-res.status(500).json({ error: "Internal Server Error" });
+    const errorDetails = {
+        name: error.name,
+        message: error.message,
+        stack: error.stack,
+        cause: error.cause,
+    };
+    console.error("Error details:", JSON.stringify(errorDetails, (key, value) => {
+        if (key === 'stack' && typeof value === 'string') {
+            return value.split('\n').map(line => line.trim());
+        }
+        return value;
+    }, 2))
+    console.error(
+        "Error details:",
+        JSON.stringify(errorDetails, (key, value) => {
+            if (key === "stack" && typeof value === "string") {
+                return value.split("\n").map((line) => line.trim());
+            }
+            return value;
+        }, 2
+        )
+    )
+    res.status(500).json({ error: "Internal Server Error" });
 }
